158becffe108bb448166e5f7602a99ed155937e7
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-08-12 13:58:47 +0300

    Update script9.js

--- script9.js
+++ script9.js
@@ -850,8 +850,6 @@ filterSalesBtn.addEventListener('click', renderSalesTransactions);
 // Variable declarations for original fields
 const inventoryForm = document.getElementById('inventory-form');
 const ingredientIdInput = document.getElementById('ingredient-id');
-const ingredientNameInput = document.getElementById('ingredient-name');
-const ingredientCostPerUnitInput = document.getElementById('ingredient-cost-per-unit');
 const cancelIngredientEditBtn = document.getElementById('cancel-ingredient-edit-btn');
 const inventoryItemsTableBody = document.getElementById('inventory-items-table-body');
 const filterInventoryBtn = document.getElementById('filter-inventory-btn');
